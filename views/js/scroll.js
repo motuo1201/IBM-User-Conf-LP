5e8d890218cd1ce765a89b8c7ae30c600201ed3c
motuo1201 <enomotu@hotmail.com>
2020-05-21 09:04:11 +0900

    ヘッダがはみ出ていたので修正

--- views/js/scroll.js
+++ views/js/scroll.js
@@ -1,10 +1,10 @@
 $(function(){
-$('a[href^="#"]').click(function() {
-var speed = 800;
-var href= $(this).attr("href");
-var target = $(href == "#" || href == "" ? 'html' : href);
-var position = target.offset().top;
-$('body,html').animate({scrollTop:position}, speed, 'swing');
-return false;
-});
+    $('a[href^="#"]').click(function() {
+    const speed = 800;
+    const href= $(this).attr("href");
+    const target = $(href == "#" || href == "" ? 'html' : href);
+    const position = target.offset().top;
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+    });
 });
